44c6c497550b44fc93e8c4a5469600e022ec5914
Steve <Steve@SFR-2018-XPS-15>
2024-08-03 13:17:01 -0400

    Added DataTable to show that problem

--- MyData.cs
+++ MyData.cs
@@ -1,9 +1,13 @@
-﻿namespace ImmediateWindow
+﻿using System.Data;
+
+namespace ImmediateWindow
     {
     public class MyData
         {
         public MyData() { }
 
         public int MyInt { get; set; }
+
+        public DataTable MyDataTable { get; set; }
         }
     }
